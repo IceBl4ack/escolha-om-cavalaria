507b2ac2a6a18f0ac6c03feb01923ccd030ba448
IceBl4ack <100386975+IceBl4ack@users.noreply.github.com>
2026-09-16 10:01:31 -0300

    Adiciona service worker

---
+++ web/sw.js
@@ -0,0 +1,9 @@
+const CACHE='escolha-om-docker-v2';
+const STATIC=['./','./index.html','./admin.html','./styles.css','./app.js','./admin.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{}))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin||u.pathname.startsWith('/api/')) return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
