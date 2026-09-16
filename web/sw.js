b61859bc7135bbcf960923884d08570761ad5cbb
IceBl4ack <100386975+IceBl4ack@users.noreply.github.com>
2026-09-16 18:21:15 -0300

    Atualiza cache PWA para controles de zoom

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='escolha-om-docker-v7';
+const CACHE='escolha-om-docker-v8';
 const STATIC=['./','./index.html','./admin.html','./styles.css','./app.js','./admin.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})())});
